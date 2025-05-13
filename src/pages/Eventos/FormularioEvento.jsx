abf5d27dc8a8d9f59c244d4f46156f4020b6ef86
rafaRM002 <156197209+rafaRM002@users.noreply.github.com>
2025-05-13 14:21:24 +0200

    Quitar optiones de formulario
    Lo he eliminado porque o tenemos nada mas que las tres opciones primeras(planificado,en progreso,finalizado)

--- src/pages/Eventos/FormularioEvento.jsx
+++ src/pages/Eventos/FormularioEvento.jsx
@@ -244,8 +244,6 @@ export default function FormularioEvento({ evento = null, onClose }) {
                 <option value="planificado">Planificado</option>
                 <option value="en progreso">En progreso</option>
                 <option value="finalizado">Finalizado</option>
-                <option value="completado">Completado</option>
-                <option value="cancelado">Cancelado</option>
               </select>
             </div>
 
